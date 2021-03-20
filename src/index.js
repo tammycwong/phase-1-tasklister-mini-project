7dfb1482e0b8d0bbe4cb4b0af5335b287751a21a
Tammy <tammycwong@gmail.com>
2021-03-20 11:41:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded"() => {
   const todoListForm = document.getElementById('create-task-form');
   const newTaskDescription = document.getElementById("new-task-description");
-  const listItem = document,getElementById("tasks");
+  const listItem = document.getElementById("tasks");
 
   todoListForm.addEventListener("submit", createNewTask);
+
+  function createNewTask(e){
+    e.preventDefault();
+    const newTask = document.createElement("li";)
+    listItem.innerText = newTaskDescription.value;
+
+    append(listItem);
+  }
   });
